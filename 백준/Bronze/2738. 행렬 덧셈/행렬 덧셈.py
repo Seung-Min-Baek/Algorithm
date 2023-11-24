669411de60f0360ade9589179b3167affe42523a
Seung-Min-Baek <140074653+Seung-Min-Baek@users.noreply.github.com>
2023-11-24 18:46:56 +0900

    Update 행렬 덧셈.py

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,6 +1,6 @@
 n,m = map(int,input().split())
 a = []
-for _ in range(n):
+for _ in range(n): # 3x3 행렬 입력을 위해 n행만큼 반복
     k=list(map(int,input().split()))
     a.append(k)
 b=[]
@@ -8,7 +8,8 @@ for _ in range(n):
     k=list(map(int,input().split()))
     b.append(k)
 
-for i in range(n):
+# 행렬 a,b 덧셈
+for i in range(n): 
   for j in range(m):
     print(a[i][j]+b[i][j],end=' ')
   print()
