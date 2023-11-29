d7e1622b66b43ef81235b77f496f4fb295636619
Seung-Min-Baek <140074653+Seung-Min-Baek@users.noreply.github.com>
2023-11-29 19:44:40 +0900

    Update 달팽이는 올라가고 싶다.py

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -2,3 +2,16 @@ A,B,V = map(int,input().split())
 
 day = (V-B)/(A-B)
 print(int(day) if day==int(day) else int(day)+1)
+
+## 처음시도 때 시간초과 된 반복문 코드
+#A,B,V = map(int,input().split())
+#num=0
+
+#while V>0:
+#  V=V-A
+#  num+=1
+#  if V==0:
+#    break
+#  V=V+B
+
+#print(num)
