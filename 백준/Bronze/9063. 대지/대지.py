5de559ead549acf3eca8df95788e0fbf3a74e4e2
Seung-Min-Baek <140074653+Seung-Min-Baek@users.noreply.github.com>
2023-12-06 16:03:18 +0900

    [Bronze III] Title: 대지, Time: 2040 ms, Memory: 35060 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+x = []
+y = []
+
+for _ in range(n):
+  x_point, y_point = map(int, input().split())
+  x.append(x_point)
+  y.append(y_point)
+
+print((max(x)-min(x))*(max(y)-min(y)))
